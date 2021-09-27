ec13a4bdd74af8b664a4931b852e774b78b4d7c3
Miguel Hiciano <mike29hiciano@gmail.com>
2021-09-27 16:31:11 -0400

    fixing car recognition

--- cv.py
+++ cv.py
@@ -1,35 +1,39 @@
 import cv2
-import numpy as np
+#import numpy as np
 
 cap = cv2.VideoCapture(0)
 object_detector = cv2.createBackgroundSubtractorMOG2()
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+cascade = cv2.CascadeClassifier("cars2.xml")
 
-while True:
-    ret, frame= cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #fc = face_cascade.load("")
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    mask = object_detector.apply(frame)
-    #contour, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for (x,y,w,h) in faces:
-        print(x,y,w,h)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        img_item = "my-image.png"
-        cv2.imwrite(img_item,roi_gray)
+try: 
+    while True:
+        ret, frame= cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #fc = face_cascade.load("")
+        obj = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=1)
+        mask = object_detector.apply(frame)
+        #contour, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        for (x,y,w,h) in obj:
+            print(x,y,w,h)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            img_item = "my-image.png"
+            cv2.imwrite(img_item,roi_gray)
 
-        color = (255,0,0)
-        stroke = 2
-        end_cord_x = x+w
-        end_cord_y = y+h
-        cv2.rectangle(frame,(x, y), (end_cord_x,end_cord_y), color, stroke)
-    cv2.imshow("frame", frame)
-    #cv2.imshow("mask", mask)
+            color = (255,0,0)
+            stroke = 2
+            end_cord_x = x+w
+            end_cord_y = y+h
+            cv2.rectangle(frame,(x, y), (end_cord_x,end_cord_y), color, stroke)
+        cv2.imshow("frame", frame)
+        cv2.imshow("mask", mask)
 
-    key = cv2.waitKey(30)
-    if key == 27:
-        break
+        key = cv2.waitKey(30)
+        if key == 27:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+except cv2.error as e:
+    print("done")
