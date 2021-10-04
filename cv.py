e429edb8cc37af0c9ce273d6f08a418c13aa7353
Miguel Hiciano <mike29hiciano@gmail.com>
2021-10-04 15:28:45 -0400

    testing

--- cv.py
+++ cv.py
@@ -13,6 +13,7 @@ try:
     while True:
         ret, frame= cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        b = cv2.resize(frame,(400,250),fx=0,fy=0, interpolation=cv2.INTER_CUBIC)
         #fc = face_cascade.load("")
         obj = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=1)
         mask = object_detector.apply(frame)
@@ -30,6 +31,7 @@ try:
             end_cord_y = y+h
             cv2.rectangle(frame,(x, y), (end_cord_x,end_cord_y), color, stroke)
         cv2.imshow("frame", frame)
+        cv2.imshow("b",b)
         cv2.imshow("mask", mask)
 
         key = cv2.waitKey(30)
