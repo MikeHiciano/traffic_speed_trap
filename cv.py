9eac3b7addb9810c6e9aea1a2143f645bb3d5d93
Miguel Hiciano <mike29hiciano@gmail.com>
2021-10-05 15:54:36 -0400

    adjusting haarcascade model

--- cv.py
+++ cv.py
@@ -1,25 +1,17 @@
-#from picamera import PiCamera
 import cv2
-#import numpy as np
-
-#camera = PiCamera()
-#camera.resolution = (500,300)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
 object_detector = cv2.createBackgroundSubtractorMOG2()
-cascade = cv2.CascadeClassifier("cars.xml")
+cascade = cv2.CascadeClassifier("cars2.xml")
 
 try: 
     while True:
         ret, frame= cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #b = cv2.resize(frame,(400,250),fx=0,fy=0, interpolation=cv2.INTER_CUBIC)
-        #fc = face_cascade.load("")
         obj = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=1)
         mask = object_detector.apply(frame)
-        #contour, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         for (x,y,w,h) in obj:
             print(x,y,w,h)
             roi_gray = gray[y:y+h, x:x+w]
@@ -33,7 +25,6 @@ try:
             end_cord_y = y+h
             cv2.rectangle(frame,(x, y), (end_cord_x,end_cord_y), color, stroke)
         cv2.imshow("frame", frame)
-        #cv2.imshow("b",b)
         cv2.imshow("mask", mask)
 
         key = cv2.waitKey(30)
